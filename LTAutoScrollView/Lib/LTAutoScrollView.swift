a61b26cdafa95b75285360f292c5ef4d2dcdbe80
gaoliutong <1282990794@qq.com>
2018-04-04 19:49:31 +0800

    访问

--- LTAutoScrollView/Lib/LTAutoScrollView.swift
+++ LTAutoScrollView/Lib/LTAutoScrollView.swift
@@ -8,52 +8,56 @@
 
 import UIKit
 
-public class LTFlowLayout: UICollectionViewFlowLayout {
-    public override init() {
+fileprivate class LTFlowLayout: UICollectionViewFlowLayout {
+    override init() {
         super.init()
     }
-    public convenience init(itemSize: CGSize) {
+    convenience init(itemSize: CGSize) {
         self.init()
         minimumLineSpacing = 0.0
         minimumInteritemSpacing = 0.0
-        scrollDirection = .horizontal
         self.itemSize = itemSize
     }
-    required public init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
-public class LTCollectionView: UICollectionView {
+fileprivate class LTCollectionView: UICollectionView {
     
-    public override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
     }
     
-    public convenience init(frame: CGRect, collectionViewLayout layout: LTFlowLayout, delegate: UICollectionViewDelegate?, dataSource: UICollectionViewDataSource?) {
+    convenience init(frame: CGRect, collectionViewLayout layout: LTFlowLayout, delegate: UICollectionViewDelegate?, dataSource: UICollectionViewDataSource?) {
         self.init(frame: frame, collectionViewLayout: layout)
-        backgroundColor = UIColor.white
         bounces = false
         showsHorizontalScrollIndicator = false
+        showsVerticalScrollIndicator = false
         isPagingEnabled = true
         self.delegate = delegate
         self.dataSource = dataSource
     }
     
-    public func scrollToItem(item: Int, section: Int) {
-        scrollToItem(at: IndexPath(item: item, section: section), at: .left, animated: false)
+    fileprivate func scrollToItem(item: Int, section: Int, at: UICollectionViewScrollPosition) {
+        scrollToItem(at: IndexPath(item: item, section: section), at: at, animated: false)
     }
     
-    
-    
-    required public init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
-public class LTCollectionViewCell: UICollectionViewCell {
+extension Notification.Name {
+    public struct LTTask {
+        static let BecomeActive = Notification.Name("UIApplicationDidBecomeActiveNotification")
+        static let Background = Notification.Name("UIApplicationDidEnterBackgroundNotification")
+    }
+}
+
+fileprivate class LTCollectionViewCell: UICollectionViewCell {
     
-    public var subView: UIView? {
+    var subView: UIView? {
         didSet {
             guard let subView = subView else { return }
             imageView.isHidden = true
@@ -61,31 +65,32 @@ public class LTCollectionViewCell: UICollectionViewCell {
         }
     }
     
-    public var image: String? {
+    var image: String? {
         didSet {
             guard let image = image else { return }
             imageView.image = UIImage(named: image)
         }
     }
+
     
-    public lazy var imageView: UIImageView = {
+    lazy var imageView: UIImageView = {
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height))
         return imageView
     }()
     
     private static let reusedCellId = "LTCollectionViewCellID"
     
-    public override init(frame: CGRect) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.addSubview(imageView)
     }
     
-    public static func itemViewWithCollectionView(_ collectionView: UICollectionView, _ indexPath: IndexPath) -> LTCollectionViewCell {
+    static func itemViewWithCollectionView(_ collectionView: UICollectionView, _ indexPath: IndexPath) -> LTCollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reusedCellId, for: indexPath) as! LTCollectionViewCell
         return cell
     }
     
-    public static func registerCellWithCollectionView(_ collectionView: UICollectionView) {
+    static func registerCellWithCollectionView(_ collectionView: UICollectionView) {
         collectionView.register(LTCollectionViewCell.self, forCellWithReuseIdentifier: reusedCellId)
     }
     
@@ -94,42 +99,69 @@ public class LTCollectionViewCell: UICollectionViewCell {
     }
 }
 
+//MARK: 轮播类型视图是自定义 或者 默认
 public enum LTAutoScrollViewType {
-    case `default`
-    case  custom
+    case `default`//默认
+    case  custom //自定义
 }
 
 public class LTAutoScrollView: UIView {
     
+    /* -------------  共有方法  ---------------- */
     public typealias AutoViewHandle = () -> [UIView]
+    
+    /*如果是默认模式`default`  直接传入images数组即可*/
     public var images: [String]? {
         didSet {
-            collectionView.reloadData()
+            setupImages(images)
         }
     }
-    public var glt_timeInterval: TimeInterval = 2.0
+    
+    /* 设置滚动时间间隔 */
+    public var glt_timeInterval: TimeInterval = 2.0 {
+        didSet {
+            setupTimer()
+        }
+    }
+    
+    /* 判断轮播类型是自定义视图custom 还是默认模式`default` */
     public var autoType: LTAutoScrollViewType = .default
+    
+    /* 设置轮播图的方向 */
+    public var scrollDirection: UICollectionViewScrollDirection = .horizontal {
+        didSet {
+            layout.scrollDirection = scrollDirection
+        }
+    }
+    
+    /* 设置系统pageContoal的熟悉 */
+    public lazy var pageControl: UIPageControl = {
+        let pageControl: UIPageControl = UIPageControl(frame: CGRect(x: 0, y: bounds.height - 20, width: bounds.width, height: 20))
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        pageControl.pageIndicatorTintColor = UIColor.blue
+        return pageControl
+    }()
+    
+    /* 设置自定义视图 */
     public var autoViewHandle: AutoViewHandle? {
         didSet {
-            guard let autoViewHandle = autoViewHandle else { return }
-            contentViews.removeAll()
-            contentViews = autoViewHandle()
-            collectionView.reloadData()
+            setupAutoViewHandle(autoViewHandle: autoViewHandle)
         }
     }
     
+    /* -------------  以下私有方法  ---------------- */
     private var contentViews: [UIView] = []
-    
     private var currentPageIndex: Int = 0
     private var timer: Timer?
-    private var glt_isAdjust: Bool = false
-
-    public lazy var layout: LTFlowLayout = {
+    private var totalsCount: Int = 0
+    
+    private lazy var layout: LTFlowLayout = {
         let layout = LTFlowLayout(itemSize: CGSize(width: bounds.width, height: bounds.height))
+        layout.scrollDirection = .horizontal
         return layout
     }()
     
-    public lazy var collectionView: LTCollectionView = {
+    private lazy var collectionView: LTCollectionView = {
         let collectionView = LTCollectionView(frame: bounds, collectionViewLayout: layout, delegate: self, dataSource: self)
         return collectionView
     }()
@@ -146,28 +178,74 @@ public class LTAutoScrollView: UIView {
 }
 
 extension LTAutoScrollView {
+    
+    private func setupImages(_ images: [String]?)  {
+        guard let images = images else { return }
+        totalsCount = images.count * 1024
+        if scrollDirection == .vertical {
+            collectionView.scrollToItem(item: totalsCount / 2, section: 0, at: .top)
+        }else {
+            collectionView.scrollToItem(item: totalsCount / 2, section: 0, at: .left)
+        }
+        pageControl.numberOfPages = images.count
+        collectionView.reloadData()
+    }
+    
+    private func setupAutoViewHandle(autoViewHandle: AutoViewHandle?) {
+        guard let autoViewHandle = autoViewHandle else { return }
+        contentViews.removeAll()
+        contentViews = autoViewHandle()
+        totalsCount = contentViews.count * 1024
+        if scrollDirection == .vertical {
+            collectionView.scrollToItem(item: totalsCount / 2, section: 0, at: .top)
+        }else {
+            collectionView.scrollToItem(item: totalsCount / 2, section: 0, at: .left)
+        }
+        pageControl.numberOfPages = contentViews.count
+        collectionView.reloadData()
+    }
+}
+
+extension LTAutoScrollView {
     private func setupSubViews() {
         LTCollectionViewCell.registerCellWithCollectionView(collectionView)
         addSubview(collectionView)
+        addSubview(pageControl)
+        registerNotification()
         setupTimer()
+    }
+    
+    private func registerNotification() {
+        let noticenter = NotificationCenter.default
+        noticenter.addObserver(self, selector: #selector(registerNoti(_:)), name: Notification.Name.LTTask.BecomeActive, object: nil)
+        noticenter.addObserver(self, selector: #selector(registerNoti(_:)), name: Notification.Name.LTTask.Background, object: nil)
+    }
+    
+    @objc private func registerNoti(_ notification: Notification) {
+        if notification.name == Notification.Name.LTTask.BecomeActive {
+            timer?.restart(glt_timeInterval)
+        }else {
+            timer?.pause()
+        }
     }
 }
 
 extension LTAutoScrollView {
     private func setupTimer() {
+        timer?.invalidate()
+        timer = nil
         timer = Timer(timeInterval: glt_timeInterval, target: self, selector: #selector(timerUpdate(_:)), userInfo: nil, repeats: true)
         RunLoop.current.add(timer!, forMode: .defaultRunLoopMode)
     }
     
     @objc func timerUpdate(_ timer: Timer)  {
-        print("timerUpdate")
-        if currentPageIndex == 0 && glt_isAdjust{
-            let X = bounds.width * CGFloat((autoType == .default ? (images?.count ?? 0) + 1 : contentViews.count + 1))
-            collectionView.setContentOffset(CGPoint(x: X, y: 0), animated: true)
+        if scrollDirection == .horizontal {
+            let point = CGPoint(x: CGFloat(currentPageIndex + 1) * bounds.width, y: 0)
+            collectionView.setContentOffset(point, animated: true)
         }else {
-            collectionView.setContentOffset(CGPoint(x: (bounds.width * CGFloat(currentPageIndex + 1)), y: 0), animated: true)
+            let point = CGPoint(x: 0, y: CGFloat(currentPageIndex + 1) * bounds.height)
+            collectionView.setContentOffset(point, animated: true)
         }
-        glt_isAdjust = true
     }
 }
 
@@ -184,9 +262,9 @@ extension LTAutoScrollView: UICollectionViewDelegate, UICollectionViewDataSource
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if autoType == .default {
-            return (images?.count ?? 0) + 2
+            return totalsCount
         }else {
-            return contentViews.count + 2
+            return totalsCount
         }
     }
     
@@ -201,30 +279,29 @@ extension LTAutoScrollView: UICollectionViewDelegate, UICollectionViewDataSource
         return cell
     }
     
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        var scrllW = bounds.size.width
+        var offsetX = scrollView.contentOffset.x
+        if scrollDirection == .vertical {
+            scrllW = bounds.size.height
+            offsetX = scrollView.contentOffset.y
+        }
+        let abs = offsetX.truncatingRemainder(dividingBy: scrllW)
+        if abs == 0 {
+            let totalCurrentIndex = Int(offsetX) / Int(scrllW)
+            currentPageIndex = totalCurrentIndex
+            let contentCount = (autoType == .default ? (images?.count ?? 1) : contentViews.count)
+            let index = totalCurrentIndex % contentCount
+            pageControl.currentPage = index
+        }
+    }
+    
     public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         timer?.pause()
-        if item(scrollView)?.item == 0 {
-            collectionView.scrollToItem(item: autoType == .default ? images?.count ?? 0 : contentViews.count, section: 0)
-        }
     }
-    
+   
     public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         timer?.restart(glt_timeInterval)
-    }
-    
-    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        scrollViewDidEndScrollingAnimation(collectionView)
-    }
-    
-    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-        guard let item = item(scrollView)?.item else { return }
-        if item == 0 {
-            collectionView.scrollToItem(item: autoType == .default ? images?.count ?? 0 : contentViews.count, section: 0)
-        }else if item == (autoType == .default ? images?.count ?? 0 : contentViews.count) + 1 {
-            collectionView.scrollToItem(item: 1, section: 0)
-        }
-        currentPageIndex = item % (autoType == .default ? images?.count ?? 0 : contentViews.count)
-        print("当前的位置 ---> \(currentPageIndex)")
     }
     
     private func item(_ scrollView: UIScrollView) -> IndexPath? {
--- LTAutoScrollView/ViewController.swift
+++ LTAutoScrollView/ViewController.swift
@@ -14,20 +14,131 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("viewDidLoad")
         self.automaticallyAdjustsScrollViewInsets = false
-        Deom1()
-        Deom2()
+        demo1()
+        demo2()
+        demo3()
+        Deom3()
+        Deom4()
+        Deom5()
+        Deom6()
     }
     
-    func Deom1() {
-        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 0, y: 80, width: view.bounds.width, height: 200))
-        view.addSubview(autoScrollView)
-        autoScrollView.images = ["cycle_image1","cycle_image2"]
-    }
     
-    func Deom2() {
-        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 0, y: 300, width: view.bounds.width, height: 200))
+    
+    
+    
+    func Deom3() {
+        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 0, y: 360, width: view.bounds.width, height: 80))
         view.addSubview(autoScrollView)
         autoScrollView.autoType = .custom
+        autoScrollView.scrollDirection = .vertical
+        autoScrollView.pageControl.isHidden = true
+        autoScrollView.autoViewHandle = {
+            var views = [UIView]()
+            for index in 1..<4 {
+                
+                let myV = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 80))
+                myV.backgroundColor = UIColor.black
+
+                let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 40))
+                label.text = "测试--\(index)"
+                label.backgroundColor = UIColor.black
+                label.textColor = UIColor.white
+                label.textAlignment = .center
+                myV.addSubview(label)
+                views.append(myV)
+                
+                let label1 = UILabel(frame: CGRect(x: 0, y: 40, width: self.view.bounds.width, height: 40))
+                label1.text = "测试--\(index)"
+                label1.backgroundColor = UIColor.black
+                label1.textColor = UIColor.white
+                label1.textAlignment = .center
+                myV.addSubview(label1)
+                views.append(myV)
+            }
+            return views
+        }
+    }
+    
+    func Deom4() {
+        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 0, y: 460, width: view.bounds.width, height: 40))
+        view.addSubview(autoScrollView)
+        autoScrollView.autoType = .custom
+        autoScrollView.scrollDirection = .vertical
+        autoScrollView.pageControl.isHidden = true
+        autoScrollView.autoViewHandle = {
+            var views = [UILabel]()
+            for index in 1..<4 {
+                let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 40))
+                label.text = "测试--\(index)"
+                label.backgroundColor = UIColor.black
+                label.textColor = UIColor.white
+                label.textAlignment = .center
+                views.append(label)
+            }
+            return views
+        }
+    }
+    
+    func Deom5() {
+        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 0, y: 520, width: view.bounds.width, height: 120))
+        view.addSubview(autoScrollView)
+        autoScrollView.autoType = .custom
+        autoScrollView.scrollDirection = .horizontal
+        autoScrollView.autoViewHandle = {
+            var views = [UIImageView]()
+            for index in 1..<4 {
+                let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 200))
+                imageView.image = UIImage(named: "cycle_image\(index)")
+                views.append(imageView)
+                
+                let label = UILabel(frame: CGRect(x: 15, y: 15, width: 80, height: 40))
+                label.text = "测试--\(index)"
+                label.textColor = UIColor.white
+                label.backgroundColor = UIColor.black
+                label.textAlignment = .center
+                imageView.addSubview(label)
+            }
+            return views
+        }
+    }
+    
+    func Deom6() {
+        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 30, y: 20, width: view.bounds.width - 60, height: 50))
+        view.addSubview(autoScrollView)
+        autoScrollView.glt_timeInterval = 0.5
+        autoScrollView.scrollDirection = .horizontal
+        autoScrollView.images = ["cycle_image1","cycle_image2"]
+    }
+}
+
+
+extension ViewController {
+    func demo1() {
+        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 30, y: 20, width: view.width - 60, height: 50))
+        view.addSubview(autoScrollView)
+        autoScrollView.glt_timeInterval = 0.5
+        autoScrollView.scrollDirection = .horizontal
+        autoScrollView.images = ["cycle_image1","cycle_image2"]
+    }
+}
+
+extension ViewController {
+    func demo2() {
+        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 0, y: 80, width: view.bounds.width, height: 120))
+        view.addSubview(autoScrollView)
+        autoScrollView.glt_timeInterval = 3
+        autoScrollView.scrollDirection = .vertical
+        autoScrollView.images = ["cycle_image1","cycle_image2"]
+    }
+}
+
+extension ViewController {
+    func demo3() {
+        let autoScrollView = LTAutoScrollView(frame: CGRect(x: 0, y: 220, width: view.bounds.width, height: 120))
+        view.addSubview(autoScrollView)
+        autoScrollView.autoType = .custom
+        autoScrollView.scrollDirection = .horizontal
         autoScrollView.autoViewHandle = {
             var views = [UIImageView]()
             for index in 1..<4 {
@@ -38,7 +149,86 @@ class ViewController: UIViewController {
             return views
         }
     }
-    
+}
 
+
+extension UIView {
+
+    public var x: CGFloat{
+        get{
+            return self.frame.origin.x
+        }
+        set{
+            var r = self.frame
+            r.origin.x = newValue
+            self.frame = r
+        }
+    }
+    
+    public var y: CGFloat{
+        get{
+            return self.frame.origin.y
+        }
+        set{
+            var r = self.frame
+            r.origin.y = newValue
+            self.frame = r
+        }
+    }
+
+    public var right: CGFloat{
+        get{
+            return self.x + self.width
+        }
+        set{
+            var r = self.frame
+            r.origin.x = newValue - frame.size.width
+            self.frame = r
+        }
+    }
+ 
+    public var bottom: CGFloat{
+        get{
+            return self.y + self.height
+        }
+        set{
+            var r = self.frame
+            r.origin.y = newValue - frame.size.height
+            self.frame = r
+        }
+    }
+    
+    public var width: CGFloat{
+        get{
+            return self.frame.size.width
+        }
+        set{
+            var r = self.frame
+            r.size.width = newValue
+            self.frame = r
+        }
+    }
+    public var height: CGFloat{
+        get{
+            return self.frame.size.height
+        }
+        set{
+            var r = self.frame
+            r.size.height = newValue
+            self.frame = r
+        }
+    }
+
+    
+    public var size: CGSize{
+        get{
+            return self.frame.size
+        }
+        set{
+            self.width = newValue.width
+            self.height = newValue.height
+        }
+    }
+    
 }
 
